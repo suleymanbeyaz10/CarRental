ca74b5fa5a4b1002b78d98db5c68b338376c8aaf
Süleyman Beyaz <suleymanbeyaz10@gmail.com>
2023-07-03 12:34:45 +0300

    autofac and validation actions applied

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty();
+            RuleFor(p => p.Name).MinimumLength(2);
+            RuleFor(p => p.DailyPrice).NotEmpty();
+            RuleFor(p => p.DailyPrice).GreaterThan(0);
+        }
+    }
+}
---
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(p => p.FirstName).NotEmpty();
+            RuleFor(p => p.FirstName).MinimumLength(2);
+            RuleFor(p => p.Password).MinimumLength(8);
+        }
+    }
+}
